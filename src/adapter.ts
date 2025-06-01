c4ebfa213b032a6ba492554c99da6b624c1560d7
Roman Andreevich <romafruit@gmail.com>
2025-06-01 18:51:53 +0300

    added persisted packets & session aware adapter

--- src/adapter.ts
+++ src/adapter.ts
@@ -5,6 +5,7 @@ import type { Socket } from './socket';
 import type { Namespace } from './namespace';
 import type { DefaultEventsMap, EventsMap } from '#types/typed-events';
 import { debugConfig } from '../config';
+import { yeast } from './socket.io-adapter/yeast';
 
 const debug = debugModule('socket.io:adapter');
 debug.enabled = debugConfig.adapter;
@@ -50,12 +51,7 @@ interface SessionToPersist {
 
 export type Session = SessionToPersist & { missedPackets: unknown[][] };
 
-export class Adapter<
-	ListenEvents extends EventsMap = DefaultEventsMap,
-	EmitEvents extends EventsMap = DefaultEventsMap,
-	ServerSideEvents extends EventsMap = DefaultEventsMap,
-	SocketData extends DefaultSocketData = DefaultSocketData,
-> extends EventEmitter {
+export class Adapter extends EventEmitter {
 	private rooms: Map<Room, Set<SocketId>> = new Map();
 	private sids: Map<SocketId, Set<Room>> = new Map();
 	private readonly encoder;
@@ -65,7 +61,7 @@ export class Adapter<
 	 *
 	 * @param {Namespace} nsp
 	 */
-	constructor(readonly nsp: Namespace<ListenEvents, EmitEvents, ServerSideEvents, SocketData>) {
+	constructor(readonly nsp: Namespace<any, any, any, DefaultSocketData>) {
 		super();
 		this.encoder = nsp.server.encoder;
 	}
@@ -412,10 +408,27 @@ export class Adapter<
 		return exceptSids;
 	}
 
-	// TODO: add cimpitability cluster
-	private serverSideEmit() {}
-	private persistSession() {}
-	private restoreSession() {}
+	/**
+	 * Send a packet to the other Socket.IO servers in the cluster
+	 * @param packet - an array of arguments, which may include an acknowledgement callback at the end
+	 */
+	public serverSideEmit(packet: any[]): void {
+		console.warn('this adapter does not support the serverSideEmit() functionality');
+	}
+
+	/**
+	 * Save the client session in order to restore it upon reconnection.
+	 */
+	public persistSession(session: SessionToPersist) {}
+
+	/**
+	 * Restore the session and find the packets that were missed by the client.
+	 * @param pid
+	 * @param offset
+	 */
+	public restoreSession(pid: PrivateSessionId, offset: string): Promise<Session> | null {
+		return null;
+	}
 }
 
 // TODO: Add serving files
@@ -460,3 +473,105 @@ export class Adapter<
 // 	res.onAborted(destroyReadStream);
 // 	readStream.on('data', onDataChunk).on('error', onError).on('end', destroyReadStream);
 // }
+
+interface PersistedPacket {
+	id: string;
+	emittedAt: number;
+	data: unknown[];
+	opts: BroadcastOptions;
+}
+
+type SessionWithTimestamp = SessionToPersist & { disconnectedAt: number };
+
+export class SessionAwareAdapter extends Adapter {
+	private readonly maxDisconnectionDuration: number;
+
+	private sessions: Map<PrivateSessionId, SessionWithTimestamp> = new Map();
+	private packets: PersistedPacket[] = [];
+
+	constructor(override readonly nsp: Namespace<any, any, any, DefaultSocketData>) {
+		super(nsp);
+		this.maxDisconnectionDuration = nsp.server['opts'].connectionStateRecovery!.maxDisconnectionDuration!;
+
+		const timer = setInterval(() => {
+			const threshold = Date.now() - this.maxDisconnectionDuration;
+			this.sessions.forEach((session, sessionId) => {
+				const hasExpired = session.disconnectedAt < threshold;
+				if (hasExpired) {
+					this.sessions.delete(sessionId);
+				}
+			});
+			for (let i = this.packets.length - 1; i >= 0; i--) {
+				const hasExpired = this.packets[i]!.emittedAt < threshold;
+				if (hasExpired) {
+					this.packets.splice(0, i + 1);
+					break;
+				}
+			}
+		}, 60 * 1000);
+		// prevents the timer from keeping the process alive
+		timer.unref();
+	}
+
+	override persistSession(session: SessionToPersist) {
+		(session as SessionWithTimestamp).disconnectedAt = Date.now();
+		this.sessions.set(session.pid, session as SessionWithTimestamp);
+	}
+
+	override restoreSession(pid: PrivateSessionId, offset: string): Promise<Session> | null {
+		const session = this.sessions.get(pid);
+		if (!session) {
+			// the session may have expired
+			return null;
+		}
+		const hasExpired = session.disconnectedAt + this.maxDisconnectionDuration < Date.now();
+		if (hasExpired) {
+			// the session has expired
+			this.sessions.delete(pid);
+			return null;
+		}
+		const index = this.packets.findIndex((packet) => packet.id === offset);
+		if (index === -1) {
+			// the offset may be too old
+			return null;
+		}
+		const missedPackets = [];
+		for (let i = index + 1; i < this.packets.length; i++) {
+			const packet = this.packets[i];
+			if (shouldIncludePacket(session.rooms, packet!.opts)) {
+				missedPackets.push(packet!.data);
+			}
+		}
+		return Promise.resolve({
+			...session,
+			missedPackets,
+		});
+	}
+
+	override broadcast(packet: any, opts: BroadcastOptions) {
+		const isEventPacket = packet.type === 2;
+		// packets with acknowledgement are not stored because the acknowledgement function cannot be serialized and
+		// restored on another server upon reconnection
+		const withoutAcknowledgement = packet.id === undefined;
+		const notVolatile = opts.flags?.volatile === undefined;
+		if (isEventPacket && withoutAcknowledgement && notVolatile) {
+			const id = yeast();
+			// the offset is stored at the end of the data array, so the client knows the ID of the last packet it has
+			// processed (and the format is backward-compatible)
+			packet.data.push(id);
+			this.packets.push({
+				id,
+				opts,
+				data: packet.data,
+				emittedAt: Date.now(),
+			});
+		}
+		super.broadcast(packet, opts);
+	}
+}
+
+function shouldIncludePacket(sessionRooms: Room[], opts: BroadcastOptions): boolean {
+	const included = opts.rooms.size === 0 || sessionRooms.some((room) => opts.rooms.has(room));
+	const notExcluded = sessionRooms.every((room) => !opts.except!.has(room));
+	return included && notExcluded;
+}
