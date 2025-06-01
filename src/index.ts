797a9731cd1c89214aad7d875237c6cdb8c62c50
Roman Andreevich <romafruit@gmail.com>
2025-06-01 16:56:43 +0300

    moved server to index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,542 @@
-// Main exports
-export { Server } from './server';
-export { Socket } from './socket';
-export { Namespace } from './namespace';
-export { BroadcastOperator } from './broadcast';
-export { Adapter } from './adapter';
-export * as parser from './socket.io-parser';
-
-// Types
-export type {
+import debugModule from 'debug';
+import base64id from 'base64id';
+import EventEmitter from 'events';
+import { Namespace, type ExtendedError, type ServerReservedEventsMap } from './namespace';
+import { BroadcastOperator } from './broadcast';
+import { Connection } from './connection';
+import { Adapter, type Room } from './adapter';
+import type { Server as BunServer, ServerWebSocketSendStatus } from 'bun';
+import type {
+	RESERVED_EVENTS,
 	ServerToClientEvents,
 	ClientToServerEvents,
-	SocketPacketFromClient,
-	SocketPacketToClient,
-	SocketId,
-	Room,
+	SocketData as DefaultSocketData,
 	AckCallback,
-	AckMap,
 	DisconnectReason,
-	Handshake,
-	SocketData,
 } from '../types/socket-types';
+import {
+	StrictEventEmitter,
+	type EventsMap,
+	type DefaultEventsMap,
+	type EventParams,
+	type EventNames,
+	type DecorateAcknowledgementsWithTimeoutAndMultipleResponses,
+	type AllButLast,
+	type Last,
+	type RemoveAcknowledgements,
+	type EventNamesWithAck,
+	type FirstNonErrorArg,
+} from '#types/typed-events';
+import { Socket } from './socket';
+import type { Context } from 'hono';
+import type { Client } from './client';
+import * as parser from './socket.io-parser';
+import type { Encoder } from './socket.io-parser';
+import { debugConfig } from '../config';
+
+const debug = debugModule('socket.io:server');
+debug.enabled = debugConfig.server;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+type MiddlewareFn<SocketData> = (socket: any, next: (err?: Error) => void) => void;
+
+type AdapterConstructor = typeof Adapter | ((nsp: Namespace) => Adapter);
+
+interface ServerOptions {
+	path: string;
+	adapter: AdapterConstructor;
+	parser: typeof parser; // not any type
+	pingInterval?: number;
+	pingTimeout?: number;
+	maxPayload?: number;
+	/**
+	 * how many ms before a client without namespace is closed
+	 * @default 45000
+	 */
+	connectTimeout: number;
+}
+
+/**
+ * Main Socket.IO Server class with full TypeScript support
+ */
+class Server<
+	ListenEvents extends EventsMap = ClientToServerEvents,
+	EmitEvents extends EventsMap = ServerToClientEvents,
+	ServerSideEvents extends EventsMap = DefaultEventsMap,
+	SocketData extends DefaultSocketData = DefaultSocketData,
+> extends StrictEventEmitter<
+	/** strict typing */
+	ServerSideEvents,
+	RemoveAcknowledgements<EmitEvents>,
+	ServerReservedEventsMap<ListenEvents, EmitEvents, ServerSideEvents, SocketData>
+> {
+	private engine?: BunServer;
+	readonly _parser: typeof parser;
+	readonly encoder: Encoder;
+
+	public readonly _nsps: Map<string, Namespace<ListenEvents, EmitEvents, ServerSideEvents, SocketData>> = new Map();
+	public readonly _clients: Map<string, Client<ListenEvents, EmitEvents, ServerSideEvents, SocketData>> = new Map();
+
+	private _path?: string;
+	private _adapter?: AdapterConstructor;
+
+	public readonly sockets: Namespace<ListenEvents, EmitEvents, ServerSideEvents, SocketData>;
+	private readonly opts: Partial<ServerOptions>;
+	/** @private */
+	_connectTimeout!: number;
+
+	/**
+	 * Server constructor.
+	 *
+	 * @param [opts]
+	 */
+	constructor(
+		opts: Partial<ServerOptions> = {
+			pingInterval: 25000,
+			pingTimeout: 20000,
+			maxPayload: 1000000,
+		},
+	) {
+		super();
+		this.path(opts.path || '/socket.io');
+		this.connectTimeout(opts.connectTimeout || 45000);
+		this._parser = opts.parser || parser;
+		this.encoder = new this._parser.Encoder();
+		this.adapter(opts.adapter || Adapter);
+		this.opts = opts;
+		this.sockets = this.of('/');
+	}
+
+	get _opts() {
+		return this.opts;
+	}
+
+	/**
+	 * Sets the client serving path.
+	 *
+	 * @param {String} v pathname
+	 * @return {Server|String} self when setting or value when getting
+	 */
+	public path(v: string): this;
+	public path(): string;
+	public path(v?: string): this | string;
+	public path(v?: string): this | string {
+		if (!arguments.length) return this._path!;
+		this._path = v!.replace(/\/$/, '');
+
+		// const escapedPath = this._path.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+		// this.clientPathRegex = new RegExp('^' + escapedPath + '/socket\\.io(\\.msgpack|\\.esm)?(\\.min)?\\.js(\\.map)?(?:\\?|$)');
+		return this;
+	}
+
+	/**
+	 * Set the delay after which a client without namespace is closed
+	 * @param v
+	 */
+	public connectTimeout(v: number): this;
+	public connectTimeout(): number;
+	public connectTimeout(v?: number): this | number;
+	public connectTimeout(v?: number): this | number {
+		if (v === undefined) return this._connectTimeout;
+		this._connectTimeout = v;
+		return this;
+	}
+
+	/**
+	 * Sets the adapter for rooms.
+	 *
+	 * @param v pathname
+	 * @return self when setting or value when getting
+	 */
+	public adapter(): AdapterConstructor | undefined;
+	public adapter(v: AdapterConstructor): this;
+	public adapter(v?: AdapterConstructor): AdapterConstructor | undefined | this {
+		if (!arguments.length) return this._adapter;
+		this._adapter = v;
+		for (const nsp of this._nsps.values()) {
+			nsp._initAdapter();
+		}
+		return this;
+	}
+
+	/**
+	 * Attaches socket.io to a bun server.
+	 *
+	 * @param srv - server instance
+	 * @param opts - options passed to engine.io
+	 * @return self
+	 */
+	attach(srv: BunServer, opts: Partial<ServerOptions> = {}): void {
+		this.engine = srv;
+	}
+
+	listen(srv: BunServer, opts: Partial<ServerOptions> = {}): void {
+		return this.attach(srv, opts);
+	}
+
+	publish(topic: string, message: string | Uint8Array): boolean {
+		if (!this.engine) {
+			console.warn('[SocketServer] Bun server not set, cannot publish');
+			return false;
+		}
+		return <ServerWebSocketSendStatus>this.engine.publish(topic, message) > 0;
+	}
+
+	/**
+	 * Called with each incoming transport connection.
+	 *
+	 * @param {Context} c
+	 * @param {SocketData} data
+	 * @return Connection
+	 * @private
+	 */
+	onconnection(c: Context, data?: SocketData): Connection<ListenEvents, EmitEvents, ServerSideEvents, SocketData> {
+		return new Connection<ListenEvents, EmitEvents, ServerSideEvents, SocketData>(base64id.generateId(), this, c, data);
+	}
+
+	/**
+	 * Looks up a namespace.
+	 *
+	 * @example
+	 * // with a simple string
+	 * const myNamespace = io.of("/my-namespace");
+	 *
+	 * // with a regex // TODO: (not compatible on this version of socket.io-bun)
+	 * const dynamicNsp = io.of(/^\/dynamic-\d+$/).on("connection", (socket) => {
+	 *   const namespace = socket.nsp; // newNamespace.name === "/dynamic-101"
+	 *
+	 *   // broadcast to all clients in the given sub-namespace
+	 *   namespace.emit("hello");
+	 * });
+	 *
+	 * @param name - nsp name
+	 * @param fn optional, nsp `connection` ev handler
+	 */
+	of(
+		/** namespace */
+		name: string,
+		fn?: (socket: Socket<ListenEvents, EmitEvents, ServerSideEvents, SocketData>) => void,
+	): Namespace<ListenEvents, EmitEvents, ServerSideEvents, SocketData> {
+		if (String(name)[0] !== '/') name = '/' + name;
+
+		let nsp = this._nsps.get(name);
+
+		if (!nsp) {
+			debug('initializing namespace %s', name);
+			nsp = new Namespace(this, name);
+			this._nsps.set(name, nsp as any);
+
+			if (name !== '/') {
+				// @ts-ignore
+				this.sockets.emitReserved('new_namespace', nsp);
+			}
+		}
+		if (fn) nsp.on('connect', fn);
+		return nsp;
+	}
+
+	/**
+	 * Closes server connection
+	 *
+	 * @param [fn] optional, called as `fn([err])` on error OR all conns closed
+	 */
+	async close(fn?: (err?: Error) => void): Promise<void> {
+		await Promise.allSettled(
+			[...this._nsps.values()].map(async (nsp) => {
+				nsp.sockets.forEach((socket) => {
+					socket._onclose('server shutting down');
+				});
+
+				await nsp.adapter.close();
+			}),
+		);
+
+		for (const nsp of this._nsps.values()) {
+			nsp.disconnectSockets(true);
+			nsp.adapter.close();
+		}
+
+		this._nsps.clear();
+		this.removeAllListeners();
+
+		fn && fn();
+	}
+
+	/**
+	 * Registers a middleware, which is a function that gets executed for every incoming {@link Socket}.
+	 *
+	 * @example
+	 * io.use((socket, next) => {
+	 *   // ...
+	 *   next();
+	 * });
+	 *
+	 * @param fn - the middleware function
+	 */
+	use(fn: MiddlewareFn<SocketData>): this {
+		this.sockets.use(fn);
+		return this;
+	}
+
+	/**
+	 * Targets a room when emitting.
+	 *
+	 * @example
+	 * // the “foo” event will be broadcast to all connected clients in the “room-101” room
+	 * io.to("room-101").emit("foo", "bar");
+	 *
+	 * // with an array of rooms (a client will be notified at most once)
+	 * io.to(["room-101", "room-102"]).emit("foo", "bar");
+	 *
+	 * // with multiple chained calls
+	 * io.to("room-101").to("room-102").emit("foo", "bar");
+	 *
+	 * @param room - a room, or an array of rooms
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	to(room: Room | Room[]): BroadcastOperator<EmitEvents, SocketData> {
+		return this.sockets.to(room);
+	}
+
+	/**
+	 * Targets a room when emitting. Similar to `to()`, but might feel clearer in some cases:
+	 *
+	 * @example
+	 * // disconnect all clients in the "room-101" room
+	 * io.in("room-101").disconnectSockets();
+	 *
+	 * @param room - a room, or an array of rooms
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	in(room: Room | Room[]): BroadcastOperator<EmitEvents, SocketData> {
+		return this.sockets.in(room);
+	}
+
+	/**
+	 * Excludes a room when emitting.
+	 *
+	 * @example
+	 * // the "foo" event will be broadcast to all connected clients, except the ones that are in the "room-101" room
+	 * io.except("room-101").emit("foo", "bar");
+	 *
+	 * // with an array of rooms
+	 * io.except(["room-101", "room-102"]).emit("foo", "bar");
+	 *
+	 * // with multiple chained calls
+	 * io.except("room-101").except("room-102").emit("foo", "bar");
+	 *
+	 * @param room - a room, or an array of rooms
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	except(room: Room | Room[]): BroadcastOperator<EmitEvents, SocketData> {
+		return this.sockets.except(room);
+	}
+
+	/**
+	 * Sends a `message` event to all clients.
+	 *
+	 * This method mimics the WebSocket.send() method.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/send
+	 *
+	 * @example
+	 * io.send("hello");
+	 *
+	 * // this is equivalent to
+	 * io.emit("message", "hello");
+	 *
+	 * @return self
+	 */
+	send(...args: EventParams<EmitEvents, 'message'>): this {
+		this.sockets.emit('message' as any, ...args);
+		return this;
+	}
+
+	/**
+	 * Sends a `message` event to all clients. Alias of {@link send}.
+	 *
+	 * @return self
+	 */
+	write(...args: EventParams<EmitEvents, 'message'>): this {
+		this.sockets.emit('message' as any, ...args);
+		return this;
+	}
+
+	// TODO: add cluster compatibility
+	private serverSideEmit() {}
+	private serverSideEmitWithAck() {}
+
+	/**
+	 * Sets the compress flag.
+	 *
+	 * @example
+	 * io.compress(false).emit("hello");
+	 *
+	 * @param compress - if `true`, compresses the sending data
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	compress(compress: boolean) {
+		return this.sockets.compress(compress);
+	}
+
+	/**
+	 * Sets a modifier for a subsequent event emission that the event data may be lost if the client is not ready to
+	 * receive messages (because of network slowness or other issues, or because they’re connected through long polling
+	 * and is in the middle of a request-response cycle).
+	 *
+	 * @example
+	 * io.volatile.emit("hello"); // the clients may or may not receive it
+	 *
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	get volatile() {
+		return this.sockets.volatile;
+	}
+
+	/**
+	 * Sets a modifier for a subsequent event emission that the event data will only be broadcast to the current node.
+	 *
+	 * @example
+	 * // the “foo” event will be broadcast to all connected clients on this node
+	 * io.local.emit("foo", "bar");
+	 *
+	 * @return a new {@link BroadcastOperator} instance for chaining
+	 */
+	get local(): BroadcastOperator<EmitEvents, SocketData> {
+		return this.sockets.local;
+	}
+
+	/**
+	 * Adds a timeout in milliseconds for the next operation.
+	 *
+	 * @example
+	 * io.timeout(1000).emit("some-event", (err, responses) => {
+	 *   if (err) {
+	 *     // some clients did not acknowledge the event in the given delay
+	 *   } else {
+	 *     console.log(responses); // one response per client
+	 *   }
+	 * });
+	 *
+	 * @param timeout
+	 */
+	timeout(timeout: number) {
+		return this.sockets.timeout(timeout);
+	}
+
+	/**
+	 * Returns the matching socket instances.
+	 *
+	 * Note: this method also works within a cluster of multiple Socket.IO servers, with a compatible {@link Adapter}.
+	 *
+	 * @example
+	 * // return all Socket instances
+	 * const sockets = await io.fetchSockets();
+	 *
+	 * // return all Socket instances in the "room1" room
+	 * const sockets = await io.in("room1").fetchSockets();
+	 *
+	 * for (const socket of sockets) {
+	 *   console.log(socket.id);
+	 *   console.log(socket.handshake);
+	 *   console.log(socket.rooms);
+	 *   console.log(socket.data);
+	 *
+	 *   socket.emit("hello");
+	 *   socket.join("room1");
+	 *   socket.leave("room2");
+	 *   socket.disconnect();
+	 * }
+	 */
+	fetchSockets(): Promise<any[]> {
+		return this.sockets.fetchSockets();
+	}
+
+	/**
+	 * Makes the matching socket instances join the specified rooms.
+	 *
+	 * Note: this method also works within a cluster of multiple Socket.IO servers, with a compatible {@link Adapter}.
+	 *
+	 * @example
+	 *
+	 * // make all socket instances join the "room1" room
+	 * io.socketsJoin("room1");
+	 *
+	 * // make all socket instances in the "room1" room join the "room2" and "room3" rooms
+	 * io.in("room1").socketsJoin(["room2", "room3"]);
+	 *
+	 * @param room - a room, or an array of rooms
+	 */
+	socketsJoin(room: Room | Room[]): void {
+		this.sockets.socketsJoin(room);
+	}
+
+	/**
+	 * Makes the matching socket instances leave the specified rooms.
+	 *
+	 * Note: this method also works within a cluster of multiple Socket.IO servers, with a compatible {@link Adapter}.
+	 *
+	 * @example
+	 * // make all socket instances leave the "room1" room
+	 * io.socketsLeave("room1");
+	 *
+	 * // make all socket instances in the "room1" room leave the "room2" and "room3" rooms
+	 * io.in("room1").socketsLeave(["room2", "room3"]);
+	 *
+	 * @param room - a room, or an array of rooms
+	 */
+	socketsLeave(room: Room | Room[]): void {
+		this.sockets.socketsLeave(room);
+	}
+
+	/**
+	 * Makes the matching socket instances disconnect.
+	 *
+	 * Note: this method also works within a cluster of multiple Socket.IO servers, with a compatible {@link Adapter}.
+	 *
+	 * @example
+	 * // make all socket instances disconnect (the connections might be kept alive for other namespaces)
+	 * io.disconnectSockets();
+	 *
+	 * // make all socket instances in the "room1" room disconnect and close the underlying connections
+	 * io.in("room1").disconnectSockets(true);
+	 *
+	 * @param close - whether to close the underlying connection
+	 */
+	disconnectSockets(close: boolean = false): void {
+		this.sockets.disconnectSockets(close);
+	}
+}
+
+/**
+ * Expose main namespace (/).
+ */
+
+const emitterMethods = Object.keys(EventEmitter.prototype).filter(function (key) {
+	// @ts-ignore
+	return typeof EventEmitter.prototype[key] === 'function';
+});
+
+emitterMethods.forEach(function (fn) {
+	// @ts-ignore
+	Server.prototype[fn] = function () {
+		// @ts-ignore
+		return this.sockets[fn].apply(this.sockets, arguments);
+	};
+});
+
+const io = Object.assign(
+	// @ts-ignore
+	(srv?: any, opts?: any) => new Server(srv, opts),
+	{
+		Server,
+		Namespace,
+		Socket,
+	},
+);
+
+export default io;
+export { Server, Socket, Namespace, BroadcastOperator };
+export type { DisconnectReason, ServerOptions, DefaultEventsMap, ExtendedError }; // RemoteSocket removed
+export type { Event } from './socket';
