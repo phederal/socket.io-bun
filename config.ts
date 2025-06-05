d5cdcf0e284abdc3ec52806ec48608fb6acfe103
Roman Andreevich <romafruit@gmail.com>
2025-06-05 19:12:58 +0300

    updated

--- config.ts
+++ config.ts
@@ -1,12 +1,18 @@
 export const debugConfig = {
-	adapter: false,
-	client: false,
-	connection: false,
-	namespace: false,
-	parentNamespace: false,
-	server: false,
-	socket: false,
-	engine: false,
-	engine_socket: false,
-	engine_transport: false,
+	adapter: as(false),
+	client: as(false),
+	connection: as(false),
+	namespace: as(false),
+	parentNamespace: as(false),
+	server: as(false),
+	socket: as(false),
+	engine: as(false),
+	engine_socket: as(false),
+	engine_transport: as(false),
 };
+
+const enabledAll = false;
+
+function as(bool: boolean) {
+	return enabledAll ?? bool;
+}
